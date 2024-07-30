cd908b3a21efb7b60521f8f16293673672ec7cc3
ParkYujin1029 <107266321+ParkYujin1029@users.noreply.github.com>
2024-07-30 13:32:50 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rkfh, tpfh;
+    cin >> rkfh >> tpfh;
+    rkfh += 8;
+    tpfh *= 3;
+    cout << rkfh << endl << tpfh << endl << rkfh*tpfh;
+    return 0;
+}
