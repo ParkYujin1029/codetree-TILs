12f7f53d42bc406404201bf655f8f5b6ff864de5
ParkYujin1029 <107266321+ParkYujin1029@users.noreply.github.com>
2024-07-31 01:33:42 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240731/동전이 있는 위치/Where-coins-are-located.cpp
+++ 240731/동전이 있는 위치/Where-coins-are-located.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    int placed[n][n] = {0};
+    int placed[n][n] = {0,};
     int x, y;
     for(int i=0; i<m; i++){
         cin >> x >> y;
