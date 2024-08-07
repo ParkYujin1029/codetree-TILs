01c5626e4365ba13605eedf4a12380e1b3d2f970
ParkYujin1029 <107266321+ParkYujin1029@users.noreply.github.com>
2024-08-07 20:15:31 +0900

    Title: 작은 구슬의 이동, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/작은 구슬의 이동/small-marble-movement.cpp
+++ 240807/작은 구슬의 이동/small-marble-movement.cpp
@@ -17,15 +17,15 @@ int main() {
     int r, c; //초기 구슬의 위치를 나타내는 정수. r행 c열에 위치함
     char d; //초기 구슬의 방향을 나타내는 문자
     cin >> r >> c >> d;
+    r --; //인덱스 형태로 변환
+    c --; //인덱스 형태로 변환
 
-    r --;
-    c --;
     int dir_num;
     if(d == 'U')dir_num = 2;
     else if(d == 'D')dir_num = 1;
     else if(d == 'R')dir_num = 0;
     else dir_num = 3;
-    int nx[4] = {0, -1, 1, 0};
+    int nx[4] = {0, 1, -1, 0};
     int ny[4] = {1, 0, 0, -1};
 
     for(int i=0; i<t; i++){
