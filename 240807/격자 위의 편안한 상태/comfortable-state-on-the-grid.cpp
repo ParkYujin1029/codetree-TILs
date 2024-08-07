35ef169cd8e136a0bc5ace15a4c3b8b5edb49040
ParkYujin1029 <107266321+ParkYujin1029@users.noreply.github.com>
2024-08-07 21:19:58 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int is_range(int N, int x, int y){
+bool is_range(int N, int x, int y){
     if(x>=N || y>=N || x<0 || y<0){
         return false;
     }else{
@@ -13,9 +13,9 @@ int is_range(int N, int x, int y){
 int main() {
     int N, M;
     cin >> N >> M;
-    int arr2[100][100] = {0}; //0은 색칠이 안된상태
-    int r[100];
-    int c[100];
+    int arr2[102][102] = {0}; //0은 색칠이 안된상태
+    int r[102];
+    int c[102];
     int nx[4] = {1, 0, -1, 0};
     int ny[4] = {0, 1, 0, -1};
     int result;
@@ -28,7 +28,11 @@ int main() {
         result = 0;
         arr2[r[i]][c[i]] = 1;
         for(int j=0; j<4; j++){
-            if(is_range(N, r[i]+nx[j], c[i]+ny[j])&&arr2[r[i]+nx[j]][c[i]+ny[j]]==1)result++;
+            int nx_cell = r[i] + nx[j];
+            int ny_cell = c[i] + ny[j];
+            if(is_range(N, nx_cell, ny_cell)&&arr2[nx_cell][ny_cell]==1){
+                result++;
+            }
         }
         if(result==3)cout<<1<<endl;
         else cout<<0<<endl;
